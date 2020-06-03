45229b668ed4c247b30d38a19640d43b3bd07b33
475815538@qq.com <475815538@qq.com>
2020-06-03 15:52:02 +0800

    负载均衡

--- springcloud/consumer-50/src/main/java/cn/ezra/springcloud/DeptConsumer50_App.java
+++ springcloud/consumer-50/src/main/java/cn/ezra/springcloud/DeptConsumer50_App.java
@@ -16,8 +16,9 @@ import org.springframework.cloud.netflix.ribbon.RibbonClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
-//在启动该微服务的时候就能去加载我们的自定义Ribbon配置类，从而使配置生效
-@RibbonClient(name = "MICROSERVICECLOUD-DEPT", configuration = MySelfRule.class)
+//在启动该微服务的时候就能去加载我们的自定义Ribbon配置类，从而使配置生效，name是指生产者服务名。
+
+@RibbonClient(name = "msc-dept", configuration = MySelfRule.class)
 public class DeptConsumer50_App {
     public static void main(String[] args) {
         SpringApplication.run(DeptConsumer50_App.class, args);
--- springcloud/consumer-50/src/main/java/cn/ezra/springcloud/controller/DeptController_Consumer.java
+++ springcloud/consumer-50/src/main/java/cn/ezra/springcloud/controller/DeptController_Consumer.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class DeptController_Consumer {
 
     //private static final String REST_URL_PREFIX = "http://localhost:8001";
-    private static final String REST_URL_PREFIX = "http://MICROSERVICECLOUD-DEPT";
+    private static final String REST_URL_PREFIX = "http://microservicecloud-hystrix-dept";
 
     /**
      * 使用 使用restTemplate访问restful接口非常的简单粗暴无脑。 (url, requestMap,
